f5f738ca9872324a446d53598b929c020661b260
baibing1304 <baibing1304@163.com>
2024-07-11 16:56:47 +0800

    Add files via upload

---
+++ chapter-3-spring-boot-web/src/main/java/demo/springboot/web/TestIssues.java
@@ -0,0 +1,21 @@
+package demo.springboot.web;
+
+public class TestIssues {
+    public static void main(String[] args) {
+        Student student1 = new Student("Alice", 20, "S001", "New York");
+        Student student2 = new Student("Bob", 22, "S002", "Los Angeles");
+
+
+        Course course1 = new Course("C001", "Java Programming");
+        Course course2 = new Course("C002", "Database Systems");
+
+        student1.enrollCourse(course1);
+        student1.enrollCourse(course2);
+        student2.enrollCourse(course1);
+
+        System.out.println(student1);
+        System.out.println(student2);
+        System.out.println(course1.getStudents());
+        System.out.println(course2.getStudents());
+    }
+}
